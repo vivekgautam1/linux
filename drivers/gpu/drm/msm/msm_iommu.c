5d3a6aa26abdefac97b1d39835236f11885b850f
Vivek Gautam <vivek.gautam@codeaurora.org>
2017-12-06 15:38:51 +0530

    drm: msm_iommu: re-enable the pm_ruuntime calls in map/unmap
    Signed-off-by: Vivek Gautam <vivek.gautam@codeaurora.org>

--- drivers/gpu/drm/msm/msm_iommu.c
+++ drivers/gpu/drm/msm/msm_iommu.c
@@ -63,9 +63,9 @@ static int msm_iommu_map(struct msm_mmu *mmu, uint64_t iova,
 	struct msm_iommu *iommu = to_msm_iommu(mmu);
 	size_t ret;
 
-//	pm_runtime_get_sync(mmu->dev);
+	pm_runtime_get_sync(mmu->dev);
 	ret = iommu_map_sg(iommu->domain, iova, sgt->sgl, sgt->nents, prot);
-//	pm_runtime_put_sync(mmu->dev);
+	pm_runtime_put_sync(mmu->dev);
 	WARN_ON(ret < 0);
 
 	return (ret == len) ? 0 : -EINVAL;
