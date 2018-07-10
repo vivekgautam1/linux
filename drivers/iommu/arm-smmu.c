99be249f51f79974303bebedece23bad7bbd11b0
Vivek Gautam <vivek.gautam@codeaurora.org>
2018-07-10 12:01:59 +0530

    iommu/arm-smmu: Error out only if not enough context interrupts
    Currently we check if the number of context banks are unequal to
    num_context_interrupts. However, there are booloaders such as, one
    on sdm845 that can mask few context banks from the kernel.
    So, although the hardware definition in device tree would mention
    the correct number of context interrupts, this number can be
    greater than the number of context banks visible to smmu in kernel.
    We should therefore error out only when the number of context banks
    is greater than the available number of context interrupts.
    
    Signed-off-by: Vivek Gautam <vivek.gautam@codeaurora.org>
    Suggested-by: Tomasz Figa <tfiga@chromium.org>
    Cc: Robin Murphy <robin.murphy@arm.com>
    Cc: Will Deacon <will.deacon@arm.com>

--- drivers/iommu/arm-smmu.c
+++ drivers/iommu/arm-smmu.c
@@ -2231,12 +2231,19 @@ static int arm_smmu_device_probe(struct platform_device *pdev)
 	if (err)
 		return err;
 
-	if (smmu->version == ARM_SMMU_V2 &&
-	    smmu->num_context_banks != smmu->num_context_irqs) {
-		dev_err(dev,
-			"found only %d context interrupt(s) but %d required\n",
-			smmu->num_context_irqs, smmu->num_context_banks);
-		return -ENODEV;
+	if (smmu->version == ARM_SMMU_V2) {
+		if (smmu->num_context_banks > smmu->num_context_irqs) {
+			dev_err(dev,
+				"found only %d context interrupt(s) but %d required\n",
+				smmu->num_context_irqs, smmu->num_context_banks);
+			return -ENODEV;
+		} else if (smmu->num_context_banks < smmu->num_context_irqs) {
+			/* truncate the extra context interrupts if there are more context banks */
+			dev_notice(dev,
+				   "found %d context interrupt(s) but only %d required\n",
+				   smmu->num_context_irqs, smmu->num_context_banks);
+			smmu->num_context_irqs = smmu->num_context_banks;
+		}
 	}
 
 	for (i = 0; i < smmu->num_global_irqs; ++i) {
